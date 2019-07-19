dc96a4882e16e5c24c3ee89ab8b1eb0bc7c6ac2b
Maftuh Ichsan <45479892+maftuh23@users.noreply.github.com>
2019-07-19 15:02:31 +0700

    first commit

---
+++ elektronik.sql
@@ -0,0 +1,195 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Jul 2019 pada 08.36
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elektronik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `login`
+--
+
+CREATE TABLE `login` (
+  `id_user` int(3) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `level` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `login`
+--
+
+INSERT INTO `login` (`id_user`, `username`, `password`, `level`) VALUES
+(1, 'admin', 'admin', 'admin'),
+(2, 'user', 'user', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `kd_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`kd_barang`, `nama_barang`, `jumlah_barang`, `harga_beli`, `harga_jual`, `tanggal_masuk`) VALUES
+('B0001', 'BESI A', 7, 5000, 10000, '2019-07-19'),
+('B0002', 'BESI B', 8, 10000, 20000, '2019-07-19'),
+('B0003', 'BESI C', 18, 10000, 50000, '2019-07-19');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_beli`
+--
+
+CREATE TABLE `tbl_beli` (
+  `nofaktur` varchar(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_beli`
+--
+
+INSERT INTO `tbl_beli` (`nofaktur`, `kd_barang`, `nama_barang`, `hsatuan`, `jumlah_beli`, `harga`, `bayar`, `kembalian`, `tanggal`) VALUES
+('F0001', 'B0001', 'BESI A', 10000, 2, 20000, 20000, 0, '2019-07-19'),
+('F0002', 'B0002', 'BESI B', 20000, 2, 40000, 40000, 0, '2019-07-19'),
+('F0003', 'B0003', 'BESI C', 50000, 2, 100000, 100000, 0, '2019-07-19'),
+('F0004', 'B0001', 'BESI A', 10000, 1, 10000, 20000, 10000, '2019-07-19');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tmp_beli`
+--
+
+CREATE TABLE `tbl_tmp_beli` (
+  `id_tmp` int(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Trigger `tbl_tmp_beli`
+--
+DELIMITER $$
+CREATE TRIGGER `batal` AFTER DELETE ON `tbl_tmp_beli` FOR EACH ROW BEGIN
+UPDATE tbl_barang SET jumlah_barang = jumlah_barang + OLD.jumlah_beli
+WHERE kd_barang = OLD.kd_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `beli` AFTER INSERT ON `tbl_tmp_beli` FOR EACH ROW BEGIN 
+UPDATE tbl_barang SET jumlah_barang = jumlah_barang - new.jumlah_beli 
+WHERE kd_barang = new.`kd_barang`; 
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indeks untuk tabel `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`kd_barang`);
+
+--
+-- Indeks untuk tabel `tbl_beli`
+--
+ALTER TABLE `tbl_beli`
+  ADD KEY `kd_barang` (`kd_barang`);
+
+--
+-- Indeks untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  ADD PRIMARY KEY (`id_tmp`),
+  ADD KEY `kd_barang` (`kd_barang`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `login`
+--
+ALTER TABLE `login`
+  MODIFY `id_user` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  MODIFY `id_tmp` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_beli`
+--
+ALTER TABLE `tbl_beli`
+  ADD CONSTRAINT `tbl_beli_ibfk_1` FOREIGN KEY (`kd_barang`) REFERENCES `tbl_barang` (`kd_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel `tbl_tmp_beli`
+--
+ALTER TABLE `tbl_tmp_beli`
+  ADD CONSTRAINT `tbl_tmp_beli_ibfk_1` FOREIGN KEY (`kd_barang`) REFERENCES `tbl_barang` (`kd_barang`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ src/uas/config.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uas;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Administrator
+ */
+public class config {
+    private static Connection conn;
+    
+    
+    public static Connection getKoneksi(){
+        
+        String  host = "jdbc:mysql://localhost/elektronik",
+                user = "root",
+                pass = "";
+        
+        try{
+            conn = (Connection) DriverManager.getConnection(host, user, pass);
+        }catch(SQLException err){
+            JOptionPane.showMessageDialog(null, err.getMessage());
+        }
+        return conn;
+    }
+}
